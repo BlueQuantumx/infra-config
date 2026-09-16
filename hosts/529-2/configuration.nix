7d27a185be008324c9c758d147062df000dc2267
Luyan Zhou <78394824+BlueQuantumx@users.noreply.github.com>
2026-09-16 22:28:29 +0800

    fixed sops key setting

--- hosts/529-2/configuration.nix
+++ hosts/529-2/configuration.nix
@@ -88,7 +88,6 @@
         export PATH=/run/current-system/sw/bin:/run/wrappers/bin
         token=$(tr -d '\n' < ${config.sops.secrets.pascal_ddns_auth.path})
         [ -n "$token" ]
-        sleep "$(od -An -N2 -tu2 /dev/urandom | awk -v max=291 '{ gsub(/[[:space:]]/, "", $0); print $0 - max * int($0 / max) }')"
         ip a | curl -fsS -X POST http://pascal08.svr.pascal-lab.net:8788/api/v1/report \
           -H "Authorization: Bearer $token" \
           -H 'Content-Type: text/plain; charset=utf-8' \
--- hosts/529-2/sops.nix
+++ hosts/529-2/sops.nix
@@ -1,10 +1,11 @@
 # sops wiring for "529-2".
 #
 # The host has its own secrets file, encrypted to the admin key and this host's
-{ ... }:
+{ inputs, ... }:
 {
+  imports = [
+    inputs.self.nixosModules.sops
+  ];
   sops.defaultSopsFile = ../../secrets/529-2.yaml;
-  sops.secrets."pascal_ddns_auth" = {
-  #   sopsFile = ../../secrets/529-2.yaml;
-  };
+  sops.secrets."pascal_ddns_auth" = { };
 }
